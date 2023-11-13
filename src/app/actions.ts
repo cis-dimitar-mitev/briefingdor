62f243d0fa64f7fa523e12bfad6a4b24f1b3632e
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-11-13 10:55:06 +0200

    Initial commit

---
+++ src/app/actions.ts
@@ -0,0 +1,6 @@
+"use server";
+
+export async function test() {
+  console.log("Test on the server");
+  return "Test";
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@mui/material";
+import styles from "./page.module.css";
+import { test } from "./actions";
+import { useState } from "react";
+
+export default function Home() {
+  const [mockState, setMockState] = useState("");
+
+  const handleServerAction = async () => {
+    const temp = await test();
+    setMockState(temp);
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        {mockState}
+
+        <Button variant="outlined" onClick={() => handleServerAction()}>
+          Test
+        </Button>
+      </div>
+    </main>
+  );
+}
