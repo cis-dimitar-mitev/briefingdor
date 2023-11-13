d65f01052da228aacaf85c295099c2fcfda9e3c4
Dimitar Mitev <dimitar.mitev@cision.com>
2023-11-13 14:44:20 +0200

    added upload file action

--- src/app/actions/openAi.ts
+++ src/app/actions/openAi.ts
@@ -1,6 +1,10 @@
 'use server';
 
 import OpenAI from 'openai';
+import * as fs from 'fs';
+import { FileObject } from 'openai/resources/files.mjs';
+import { Stream } from 'stream';
+import { FsReadStream } from 'openai/_shims/index.mjs';
 
 const openai = new OpenAI();
 
@@ -11,4 +15,20 @@ export async function main() {
     });
 
     return chatCompletion;
+}
+
+
+/**
+ * Uploads a file to OpenAI's file storage service.
+ * @param fileStream A readable stream of the file to upload.
+ * @param purpose The purpose of the file upload. Can be "assistants" or "fine-tune". Defaults to "assistants".
+ * @returns A Promise that resolves to a FileObject representing the uploaded file.
+ */
+export async function uploadFile(fileStream: FsReadStream, purpose: "assistants" | "fine-tune" = "assistants"): Promise<FileObject> {
+    const file = await openai.files.create({
+        file: fileStream,
+        purpose: purpose,
+    });
+
+    return file;
 }
