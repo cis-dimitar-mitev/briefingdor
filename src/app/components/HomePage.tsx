02a2585fc9c3b023e500ae577b4d58f4256c1bd9
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-11-13 17:15:10 +0200

    Integrate start form with BE

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import React, { useState } from 'react';
+import { useState } from "react";
+import { check } from "../actions/openAi";
 import {
   Checkbox,
   FormGroup,
   FormControlLabel,
   Button,
-  CircularProgress
-} from '@mui/material';
-import styles from '../page.module.css';
+  CircularProgress,
+} from "@mui/material";
+import styles from "../page.module.css";
 
 const HomePage = () => {
-  const [shouldCheckGrammarMistakes, setShoudCheckGrammarMistakes] = useState<boolean>(false);
-  const [shouldCheckSpellingMistakes, setShouldCheckSpellingMistakes] = useState<boolean>(false);
-  const [shouldCheckPunctuation, setShoudCheckPunctuation] = useState<boolean>(false);
+  const [shouldCheckGrammarMistakes, setShoudCheckGrammarMistakes] =
+    useState<boolean>(false);
+  const [shouldCheckSpellingMistakes, setShouldCheckSpellingMistakes] =
+    useState<boolean>(false);
+  const [shouldCheckPunctuation, setShoudCheckPunctuation] =
+    useState<boolean>(false);
   const [improveWordChoice, setImproveWordChoice] = useState<boolean>(false);
   const [shouldRewrite, setShouldRewrite] = useState<boolean>(false);
   const [primaryText, setPrimaryText] = useState<string>("");
@@ -23,43 +27,78 @@ const HomePage = () => {
   const handleAddText = (event: any) => {
     const { value } = event.target;
     setPrimaryText(value);
-  }
+  };
 
-  const handleCheckText = () => {
+  const handleCheckText = async () => {
     setIsLoading(true);
-
-    setTimeout(() => {
-      setIsLoading(false);
-      setOutput(primaryText);
-    }, 2000)
-  }
+    const checkedText = await check(
+      primaryText,
+      shouldCheckGrammarMistakes,
+      shouldCheckSpellingMistakes,
+      shouldCheckPunctuation,
+      improveWordChoice
+    );
+    setOutput(checkedText || "");
+    setIsLoading(false);
+  };
 
   return (
     <>
       <div className={styles.checkboxesContainer}>
         <FormGroup>
           <FormControlLabel
-            control={<Checkbox checked={shouldCheckGrammarMistakes} onClick={() => setShoudCheckGrammarMistakes(!shouldCheckGrammarMistakes)} />}
+            control={
+              <Checkbox
+                checked={shouldCheckGrammarMistakes}
+                onClick={() =>
+                  setShoudCheckGrammarMistakes(!shouldCheckGrammarMistakes)
+                }
+              />
+            }
             label="Grammar mistakes​"
           />
 
           <FormControlLabel
-            control={<Checkbox checked={shouldCheckSpellingMistakes} onClick={() => setShouldCheckSpellingMistakes(!shouldCheckSpellingMistakes)} />}
+            control={
+              <Checkbox
+                checked={shouldCheckSpellingMistakes}
+                onClick={() =>
+                  setShouldCheckSpellingMistakes(!shouldCheckSpellingMistakes)
+                }
+              />
+            }
             label="Spelling mistakes​"
           />
 
           <FormControlLabel
-            control={<Checkbox checked={shouldCheckPunctuation} onClick={() => setShoudCheckPunctuation(!shouldCheckPunctuation)} />}
+            control={
+              <Checkbox
+                checked={shouldCheckPunctuation}
+                onClick={() =>
+                  setShoudCheckPunctuation(!shouldCheckPunctuation)
+                }
+              />
+            }
             label="Punctuation"
           />
 
           <FormControlLabel
-            control={<Checkbox checked={improveWordChoice} onClick={() => setImproveWordChoice(!improveWordChoice)} />}
+            control={
+              <Checkbox
+                checked={improveWordChoice}
+                onClick={() => setImproveWordChoice(!improveWordChoice)}
+              />
+            }
             label="Word choice improvements"
           />
 
           <FormControlLabel
-            control={<Checkbox checked={shouldRewrite} onClick={() => setShouldRewrite(!shouldRewrite)} />}
+            control={
+              <Checkbox
+                checked={shouldRewrite}
+                onClick={() => setShouldRewrite(!shouldRewrite)}
+              />
+            }
             label="Rewrite to reduce plagiarism"
           />
         </FormGroup>
@@ -67,22 +106,29 @@ const HomePage = () => {
         <div className={styles.textAreaContainer}>
           <div className={styles.textAreaDiv}>
             <h4 className={styles.textAreaHeading}>Add your text</h4>
-            <textarea value={primaryText} className={styles.textAreaSummary} rows={20} onChange={handleAddText} />
+            <textarea
+              value={primaryText}
+              className={styles.textAreaSummary}
+              rows={20}
+              onChange={handleAddText}
+            />
           </div>
 
           <div className={styles.textAreaDiv}>
             <h4 className={styles.textAreaHeading}>Output</h4>
 
-            {
-              isLoading
-                ?
-                (<div className={styles.spinner}>
-                  <CircularProgress size={100} />
-                </div>)
-                :
-                (<textarea value={output} rows={20} className={styles.textAreaSummary} onChange={handleAddText} />)
-            }
-
+            {isLoading ? (
+              <div className={styles.spinner}>
+                <CircularProgress size={100} />
+              </div>
+            ) : (
+              <textarea
+                value={output}
+                rows={20}
+                className={styles.textAreaSummary}
+                onChange={handleAddText}
+              />
+            )}
           </div>
         </div>
 
@@ -92,6 +138,6 @@ const HomePage = () => {
       </div>
     </>
   );
-}
+};
 
 export default HomePage;
