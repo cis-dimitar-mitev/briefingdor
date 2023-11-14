137f82bf20fdb55f8ef8f8866347f3808bffd65d
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-11-14 12:18:07 +0200

    Hack 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import styles from "./page.module.css";
 import HomePage from "./components/HomePage";
 import { check, plagiarism } from "./actions/openAi";
 
+export const maxDuration = 300;
+
 export default function Home() {
   return (
     <main className={styles.main}>
